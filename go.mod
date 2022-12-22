52341a325326ad340ef5bec332c753602c41d170
Mikhail Postnikov <postnikovmu@mail.ru>
2022-12-22 13:46:59 +0300

    gopdf

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/postnikovmu/gopdf-service
+
+go 1.18
+
+require (
+	github.com/phpdave11/gofpdi v1.0.11 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/signintech/gopdf v0.15.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/signintech/gopdf"
+)
+
+func pdf_hello(res http.ResponseWriter, req *http.Request) {
+	pdf := gopdf.GoPdf{}
+	pdf.Start(gopdf.Config{PageSize: *gopdf.PageSizeA4})
+	pdf.AddPage()
+	err := pdf.AddTTFFont("wts11", ".\\ttf\\Anonymous_Pro.ttf")
+	if err != nil {
+		log.Print(err.Error())
+		return
+	}
+
+	err = pdf.SetFont("wts11", "", 14)
+	if err != nil {
+		log.Print(err.Error())
+		return
+	}
+	pdf.SetTextColor(156, 197, 140)
+
+	pdf.SetXY(10, 10)
+	pdf.Cell(nil, "Привет из gopdf")
+
+	pdf.SetLineWidth(2)
+	pdf.SetLineType("dashed")
+	pdf.Line(10, 30, 585, 30)
+
+	pdf.SetXY(10, 40)
+	pdf.Cell(nil, "Hello from gopdf")
+
+	res.Header().Set("Content-type", "application/pdf")
+	pdf.Write(res)
+}
+
+func main() {
+
+	http.HandleFunc("/", pdf_hello)
+	//http.ListenAndServe("localhost:8080", nil) //locally
+	http.ListenAndServe(":8080", nil) //SAP BTP CloudFounry
+
+}
